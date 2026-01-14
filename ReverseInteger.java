3463f401d8e4a714768360842678d53f86d66d84
hafeez-07 <hafeezmohammad689@gmail.com>
2026-01-14 08:04:34 +0530

    Day 14 : Reverse Integer

---
+++ ReverseInteger.java
@@ -0,0 +1,27 @@
+
+public class ReverseInteger {
+
+    public static int reverse(int x) {
+        int reverse = 0;
+        while (x != 0) {
+            int digit = x % 10;
+
+            if (reverse > Integer.MAX_VALUE / 10 || reverse == Integer.MAX_VALUE/10 && digit > 7) {
+                return 0;
+            } else if (reverse < Integer.MIN_VALUE / 10 || reverse == Integer.MAX_VALUE/10 && digit < -8) {
+                return 0;
+            } else {
+                reverse = reverse * 10 + digit;
+                x /= 10;
+            }
+
+        }
+        return reverse;
+    }
+
+    public static void main(String[] args) {
+        int x = 1534236469;
+        int result = reverse(x);
+        System.out.println(result);
+    }
+}
