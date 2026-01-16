7c8a3563a54c3507628eed18556112b55f38b9bd
hafeez-07 <hafeezmohammad689@gmail.com>
2026-01-16 08:15:46 +0530

    Rotate Array : Right Rotation

---
+++ RotateArray.java
@@ -0,0 +1,37 @@
+
+public class RotateArray {
+
+    public static void rotate(int nums[], int k) {
+
+        if (nums.length <= 1) {
+            return;
+        }
+
+        k = k % nums.length;
+
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void reverse(int nums[], int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2};
+        int k = 2;
+        rotate(nums, k);
+    }
+}
