d8f84d5cb8bbd47253e54d3089480e4d73608043
hafeez-07 <hafeezmohammad689@gmail.com>
2026-01-19 07:25:26 +0530

    Day 19 : Valid phrase palindrome

---
+++ ValidPalindrome.java
@@ -0,0 +1,33 @@
+
+public class ValidPalindrome {
+
+    public static boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int start = 0, last = s.length() - 1;
+        while (start < last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst)) {
+                start++;
+            } else if (!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        boolean result = isPalindrome(s);
+        System.out.println(result);
+    }
+}
